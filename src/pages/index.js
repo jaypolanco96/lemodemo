6afcf2ce0609ff3a99fd1626e1d0b9e54a3bfc50
jaypolanco96 <jaypolanco96@gmail.com>
2020-10-29 03:01:36 -0400

    css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,57 @@
 import React from "react";
-
+import favicon from "../favicon.png";
 function Home() {
   return (
     <div className="container">
       <div className="heading">
-        <h2>Rebase in 12:16</h2>
+        <img id="headingimg" src={favicon} alt="favicon"></img>
+      </div>
+      <div className="headingtitle">
+        <h1 className="title">ChefLemo is Ready</h1>
+        <h3 className=" description">
+          Stake LemonSwap LP tokens to claim your very own $GUAVA
+        </h3>
       </div>
       <style jsx>{`
         .container {
           display: flex;
+          flex-direction: column;
+          width: 98%;
           justify-content: center;
           margin: 10px;
-          box-shadow: 0px 1px 10px #000;
           border-radius: 15px;
+        }
+        .title {
+          font-family: "Kaushan Script", sans-serif;
+          color: rgb(91, 57, 38);
+          font-size: 36px;
+          text-align: center;
+          font-weight: 700;
+          margin: 0px;
+          padding: 0px;
+        }
+        .heading {
+          display: flex;
+          justify-content: center;
+        }
+        .headingtitle {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+        }
+        .description {
+            color: rgb(170, 149, 133);
+    font-size: 18px;
+    font-weight: 400;
+    margin: 0px;
+    padding: 0px;
+    text-align: center; 
+        }
+
+        #headingimg {
+          display: flex;
+          height: 130px;
         }
       `}</style>
     </div>
