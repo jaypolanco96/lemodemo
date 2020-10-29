26e3a3e94b6b6031cdbe25b0f1c7694af7623d2d
jaypolanco96 <jaypolanco96@gmail.com>
2020-10-29 02:35:59 -0400

    css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+  return (
+    <div className="container">
+      <div className="heading">
+        <h2>Rebase in 12:16</h2>
+      </div>
+      <style jsx>{`
+        .container {
+          display: flex;
+          justify-content: center;
+          margin: 10px;
+          box-shadow: 0px 1px 10px #000;
+          border-radius: 15px;
+        }
+      `}</style>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
