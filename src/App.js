3ba69f0a3f095d698e49ded97687ce8495ae7e5e
jaypolanco96 <jaypolanco96@gmail.com>
2020-10-29 02:15:18 -0400

    v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Test from './pages/test'
+import Home from './pages/index'
+import favicon from './favicon.png'
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="nav">
+          <Link to="/"><img src={favicon} alt="favicon"></img></Link>
+          <Link to="/"><h3 id="home">Home</h3></Link>
+          <Link to="/test"><h3>About</h3></Link>
+          <Link to="/test"><button><h3>Unlock Wallet</h3></button></Link>
+        </div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/test">
+            <Test />
+          </Route>
+          <Route path="/test">
+            <Test />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
